7d65fbb4b6c02af662e8b1c8d70cf6b36a464e8f
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-10-24 21:37:18 +0700

    CRUD_RESTAPI

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const todoRoutes = require('./routes/todo.js');
+const router = require('./routes/todo.js');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 - Page Not Found');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
